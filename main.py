b5d6d101d23b3c7d8fbdbeae0c7ef732e2cda9e3
tjthejuggler <tjthejuggler@gmail.com>
2021-02-18 18:58:55 -0600

    minor

--- main.py
+++ main.py
@@ -4,6 +4,12 @@ import speech_recognition as sr
 from os import path
 from pydub import AudioSegment
 import os
+import genanki
+import time
+import re
+#make it not have to create the chunks if we dont want chunks created/exported
+
+
 #import audiodiff
 #2,7,8,9,10,11 - needs checked
 #maybe language detection to find when the chime is
@@ -13,7 +19,7 @@ createTranscription = False
 makeMod = False
 createChunks = False
 onlyCertainLessons = True
-specific_foldernames = ['02chunks']
+specific_foldernames = ['03chunks']
 make_anki = True
 
 
@@ -21,12 +27,6 @@ def create_output_file(filename, output_lines):
 	with open(filename + '.txt', 'w') as f:
 		for item in output_lines:
 			f.write("%s\n" % item)
-
-
-
-deckName = lines[0].replace(' ','_')
-
-deck = genanki.Deck(round(time.time()), deckName)
 
 deck_model = genanki.Model(
 	163335419,
@@ -51,7 +51,7 @@ deck_model = genanki.Model(
 		}
 	])
 
-def create_anki_deck(my_deck, all_audio_files):
+def create_anki_deck(my_deck, all_audio_files, deckName):
 	my_package = genanki.Package(deck)
 	my_package.media_files = all_audio_files
 	my_package.write_to_file(deckName+'.apkg')
@@ -61,17 +61,20 @@ def create_anki_note(word, translation, hint, tag, url, all_audio_files, mp3_nam
 	my_note = genanki.Note(
 		model=deck_model,
 		tags=[tag],
-		fields=[word + ' ('+str(round(time.time()))+')', translation, hint, url, '[sound:'+audio_file+']'])
+		fields=[word + ' ('+str(round(time.time()))+')', translation, hint, url, '[sound:'+mp3_name+']'])
 	return my_note, all_audio_files
-	
-def create_anki(filename, chunks_foldername, bell): #get rid of the [0], [1] stuff
+#add on to chunk names so they are more individualistic
+def create_anki(filename, chunks_foldername, bell, deckName): #get rid of the [0], [1] stuff
 	file = open( cwd+"/transcriptions/"+filename+"_transcription.txt", "r")
 	lines = file.readlines()
 	file.close()
 	all_audio_files = []
-	for i in range((bell+1), len(lines)):
+	for i in range(len(lines)):
 		if ' - ' in lines[i]:
 			line = lines[i]
+			mp3_number = bell + ((i+1)*3)
+			tag = 'Turkish101'+filename.split(' - ')[2]
+			mp3_name = 'chunk'+str(mp3_number)+'.mp3'			
 			split_line = line.split(' - ')
 			foreign_word = split_line[0].strip('\n')
 			foreign_word = re.sub(r'[^\w\s]','',foreign_word)
@@ -79,12 +82,16 @@ def create_anki(filename, chunks_foldername, bell): #get rid of the [0], [1] stu
 			hint = ""
 			if len(split_line) > 2:
 				hint = split_line[2]
+			url = ' '
+			print(chunks_foldername)
 			note, all_audio_files = create_anki_note(foreign_word, translation, hint, tag, url, all_audio_files, mp3_name, chunks_foldername)
 			deck.add_note(note)
-
-	create_anki_deck(deck, all_audio_files)
+	create_anki_deck(deck, all_audio_files, deckName)
 
 for filename in os.listdir(cwd+'/sources'):
+	filename_num = filename.split(' - ')[2].split('.')[0]
+	deckName = 'wordpower101_'+filename_num
+	deck = genanki.Deck(round(time.time()), deckName)
 	chunks_foldername = filename.split('.mp3')[0]+"chunks"
 	print(chunks_foldername.split(' - ')[2])
 	if onlyCertainLessons:
@@ -94,6 +101,9 @@ for filename in os.listdir(cwd+'/sources'):
 	if chunks_foldername == 'Learn Turkish - Word Power 101 - 07chunks':
 		bell = 3
 		print('in 7')
+	else:
+		bell = 2
+	#if createChunks:
 	if not os.path.exists(cwd+'/'+chunks_foldername):
 		os.makedirs(cwd+'/'+chunks_foldername)
 
@@ -122,44 +132,48 @@ for filename in os.listdir(cwd+'/sources'):
 	for i, chunk in enumerate(chunks):
 		if createChunks:
 			chunk.export(cwd+'/'+chunks_foldername+"/chunk{0}.mp3".format(i), format="mp3")
-		sound = AudioSegment.from_mp3(cwd+'/'+chunks_foldername+"/chunk{0}.mp3".format(i))
-		sound.export("transcript.wav", format="wav")
-		AUDIO_FILE = "transcript.wav"
-		if i > bell:
-			output_chunks += chunk
-			if (i - bell) % 3 != 1:
-				output_chunks += chunk
-				short_silence = AudioSegment.silent(duration=sound.duration_seconds*1000)
-				output_chunks += short_silence
-				if (i - bell) % 3 != 0:
+
+	chunk_count = len([name for name in os.listdir('.') if os.path.isfile(name)])
+	if makeMod or createTranscription:
+		for i in range(chunk_count):
+			sound = AudioSegment.from_mp3(cwd+'/'+chunks_foldername+"/chunk{0}.mp3".format(i))
+			sound.export("transcript.wav", format="wav")
+			AUDIO_FILE = "transcript.wav"
+			if i > bell:
+				output_chunks += sound
+				if (i - bell) % 3 != 1:
+					output_chunks += sound
+					short_silence = AudioSegment.silent(duration=sound.duration_seconds*1000)
+					output_chunks += short_silence
+					if (i - bell) % 3 != 0:
+						if createTranscription:
+							r = sr.Recognizer()
+							with sr.AudioFile(AUDIO_FILE) as source:
+								audio = r.record(source)  # read the entire audio file    
+								try:              
+									transcription = r.recognize_google(audio, language="tr-TR")
+								except:
+									transcription = '???'
+								print('tr', transcription)
+								current_transcription_line =  transcription + ' - ' + current_transcription_line + ' - no hint'
+								full_transcription += [current_transcription_line] 
+								current_transcription_line = ''
+				else:
+					long_silence = AudioSegment.silent(duration=sound.duration_seconds*2000)
+					output_chunks += long_silence
 					if createTranscription:
 						r = sr.Recognizer()
 						with sr.AudioFile(AUDIO_FILE) as source:
 							audio = r.record(source)  # read the entire audio file    
 							try:              
-								transcription = r.recognize_google(audio, language="tr-TR")
+								transcription = r.recognize_google(audio, language="en-EN")
 							except:
 								transcription = '???'
-							print('tr', transcription)
-							current_transcription_line =  transcription + ' - ' + current_transcription_line + ' - no hint'
-							full_transcription += [current_transcription_line] 
-							current_transcription_line = ''
-			else:
-				long_silence = AudioSegment.silent(duration=sound.duration_seconds*2000)
-				output_chunks += long_silence
-				if createTranscription:
-					r = sr.Recognizer()
-					with sr.AudioFile(AUDIO_FILE) as source:
-						audio = r.record(source)  # read the entire audio file    
-						try:              
-							transcription = r.recognize_google(audio, language="en-EN")
-						except:
-							transcription = '???'
-						print('en', transcription)
-						current_transcription_line = transcription
+							print('en', transcription)
+							current_transcription_line = transcription
 	if createTranscription:
 		create_output_file(cwd+"/transcriptions/"+filename+"_transcription", full_transcription)
 	if makeMod:          
 		output_chunks.export(cwd+"/mods/"+filename+"_mod.mp3", format="mp3")
 	if make_anki:
-		create_anki(filename, chunks_foldername, bell)
+		create_anki(filename, chunks_foldername, bell, deckName)
